e1adf0831d520974df2fcbda595d11a4f3d5ce10
Luiz Heming <lg.luiz1708@gmail.com>
2016-05-23 17:36:16 -0300

    Alterado o nome da classe Settings

--- src/android/Settings.java
+++ src/android/Settings.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class CDVSettings extends CordovaPlugin {
+public class Settings extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
